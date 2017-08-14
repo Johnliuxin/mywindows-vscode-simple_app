095a4e7b3af7c7299f3d79e3e13e011caead6c2d
John.LiuXin <emailofliuxin@126.com>
2017-08-14 06:26:00 +0000

    Initial commit : add a c file and a Makefile
    Signed-off-by: John.LiuXin <emailofliuxin@126.com>

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ *
+ * Copyright (c) 2016  Embest-tech, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ */
+#include <stdio.h>
+
+/*
+ * Main function
+ */
+int main(int argc, char *argv[])
+{
+	printf("main function\n");
+	return 0;
+}
